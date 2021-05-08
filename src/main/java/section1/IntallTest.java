5c1d23870e0fcb962496ea127735ff1cb36b5ed0
Kumar Abhishek <mail2kumar.2021@gmail.com>
2021-05-08 23:26:18 +0400

    df

---
+++ src/main/java/section1/IntallTest.java
@@ -0,0 +1,10 @@
+package section1;
+
+import org.apache.beam.sdk.Pipeline;
+
+public class IntallTest {
+	public static void main(String[] args) {
+		Pipeline p = Pipeline.create();
+		System.out.println();
+	}
+}
